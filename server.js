724dd2247ae55d3b0e50d0de73cc3f3fd829c8b1
Alexander Bridge <alexanderchrist203@gmail.com>
2025-12-11 02:08:27 +0100

    feat: update allowedOrigins to include localhost for development

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set("trust proxy", 1);
 
 const allowedOrigins = process.env.NODE_ENV === 'production' 
   ? [
-      '',
+      'http://localhost:5173',
       process.env.FRONTEND_URL 
     ].filter(Boolean)
   : ['http://localhost:3001', 'http://10.88.34.131:3000'];
