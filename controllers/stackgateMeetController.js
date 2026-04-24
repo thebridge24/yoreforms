9e0ecd3752f3d2f225dfb507a9c3e8b8fc57d7c8
Perspicacious <johnayomide920@gmail.com>
2026-04-24 11:35:05 +0100

    updated

--- controllers/stackgateMeetController.js
+++ controllers/stackgateMeetController.js
@@ -279,7 +279,7 @@ Automated System Notification
     const emailResponse = await resend.emails.send({
       from: "StackGate International <forms@yoreflow.online>",
       to: [client_email],
-      bcc: ["stackgateinternational@gmail.com", "johnayomide920@gmail.com"],
+      bcc: ["stackgateinternational@gmail.com", "yoreforms@gmail.com"],
       subject: `Consultation Confirmation - ${reservationCode}`,
       html: htmlTemplate,
       text: textContent,
@@ -564,7 +564,7 @@ Automated System Notification
     const emailResponse = await resend.emails.send({
       from: "StackGate International <forms@yoreflow.online>",
       to: [client_email],
-      bcc: ["stackgateinternational@gmail.com", "johnayomide920@gmail.com"],
+      bcc: ["stackgateinternational@gmail.com", "yoreforms@gmail.com"],
       subject: `Booking Confirmed - Consultation ${finalReservationCode}`,
       html: htmlTemplate,
       text: textContent,
